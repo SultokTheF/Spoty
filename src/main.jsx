0a64be33793ca81b8a54ce51da8f34e37dba4f9c
SultokTheF <sultok.003@gmail.com>
2024-05-17 02:23:03 +0500

    set environment

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+import App from "./App";
+
+import "./assets/global.css";
+
+const rootNode = document.getElementById("root");
+const root = ReactDOM.createRoot(rootNode);
+
+root.render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-)
+  </BrowserRouter>
+);
